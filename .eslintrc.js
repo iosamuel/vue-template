694a2667278c2e903f81f7061c240a6e2ee18dc1
Samuel Burbano <me@iosamuel.dev>
2023-05-22 10:10:28 -0500

    feat: scaffold 🔩

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error',
+    "vue/multi-word-component-names": "off",
+  },
+};
